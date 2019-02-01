1a14f2cb77a005143f97234b4f1865f239336337
Oleg Marchenko <omarchenko.150@gmail.com>
2019-02-01 22:37:43 +0200

    Implementing a based class for all data access objects

---
+++ src/main/java/com/github/marchenkoprojects/prettyjdbc/spring5/dao/support/DaoSupport.java
@@ -0,0 +1,44 @@
+package com.github.marchenkoprojects.prettyjdbc.spring5.dao.support;
+
+import com.github.marchenkoprojects.prettyjdbc.SessionFactory;
+import com.github.marchenkoprojects.prettyjdbc.session.Session;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.util.Assert;
+
+/**
+ * Convenient super class to support all data access objects.
+ * Requires a {@link SessionFactory} to be set, providing the current {@link Session} object.
+ *
+ * @author Oleg Marchenko
+ */
+
+public abstract class DaoSupport implements InitializingBean {
+
+    private SessionFactory sessionFactory;
+
+    /**
+     * Sets an active session factory.
+     *
+     * @param sessionFactory active session factory
+     */
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    /**
+     * Retrieves and returns the current {@link Session} object.
+     *
+     * @return the current session
+     */
+    public Session getSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        Assert.notNull(sessionFactory, "Property 'sessionFactory' is required");
+    }
+}
