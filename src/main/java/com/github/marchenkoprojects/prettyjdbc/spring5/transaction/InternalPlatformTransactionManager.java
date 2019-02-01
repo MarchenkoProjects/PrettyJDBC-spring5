3a4afd828dbea70030c42398e151d71a3360df6e
Oleg Marchenko <omarchenko.150@gmail.com>
2019-02-01 23:04:02 +0200

    First implementation of a platform transaction manager for declarative transaction management

---
+++ src/main/java/com/github/marchenkoprojects/prettyjdbc/spring5/transaction/InternalPlatformTransactionManager.java
@@ -0,0 +1,74 @@
+package com.github.marchenkoprojects.prettyjdbc.spring5.transaction;
+
+import com.github.marchenkoprojects.prettyjdbc.SessionFactory;
+import com.github.marchenkoprojects.prettyjdbc.session.Session;
+import com.github.marchenkoprojects.prettyjdbc.transaction.Transaction;
+import com.github.marchenkoprojects.prettyjdbc.transaction.TransactionIsolationLevel;
+import com.github.marchenkoprojects.prettyjdbc.transaction.TransactionStatus;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionException;
+import org.springframework.transaction.support.AbstractPlatformTransactionManager;
+import org.springframework.transaction.support.DefaultTransactionStatus;
+
+/**
+ * Standard {@link org.springframework.transaction.PlatformTransactionManager} implementation
+ * for a single specific {@link com.github.marchenkoprojects.prettyjdbc.SessionFactory}.
+ *
+ * @author Oleg Marchenko
+ */
+
+public class InternalPlatformTransactionManager extends AbstractPlatformTransactionManager {
+
+    private SessionFactory sessionFactory;
+
+    /**
+     * Sets an active session factory.
+     *
+     * @param sessionFactory active session factory
+     */
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected Object doGetTransaction() throws TransactionException {
+        Session currentSession = sessionFactory.getCurrentSession();
+        Transaction currentTransaction = currentSession.getTransaction();
+        if (currentTransaction == null || currentTransaction.getStatus() == TransactionStatus.COMPLETED) {
+            return currentSession.newTransaction();
+        }
+        return currentTransaction;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void doBegin(Object transactionObject, TransactionDefinition transactionDefinition) throws TransactionException {
+        Transaction transaction = (Transaction) transactionObject;
+        transaction.setReadOnly(transactionDefinition.isReadOnly());
+        transaction.setIsolationLevel(TransactionIsolationLevel.valueOf(transactionDefinition.getIsolationLevel()));
+        transaction.begin();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void doCommit(DefaultTransactionStatus transactionStatus) throws TransactionException {
+        Transaction transaction = (Transaction) transactionStatus.getTransaction();
+        transaction.commit();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void doRollback(DefaultTransactionStatus transactionStatus) throws TransactionException {
+        Transaction transaction = (Transaction) transactionStatus.getTransaction();
+        transaction.rollback();
+    }
+}
